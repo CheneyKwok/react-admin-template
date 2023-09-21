e174e93b4d8c52a894ccdb7c048198c60048a2a2
guozhicheng <2399214024@qq.com>
2023-09-22 00:52:45 +0800

    feat: scss 配置

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from '@/App.tsx'
 import zhCN from 'antd/locale/zh_CN'
 import { ConfigProvider } from 'antd'
 import 'virtual:svg-icons-register'
+import '@/styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -21,4 +21,13 @@ export default defineConfig({
             '@': path.resolve('./src'), // 相对路径别名配置，使用 @ 代替 src
         },
     },
+
+    css: {
+        preprocessorOptions: {
+            scss: {
+                javascriptEnabled: true,
+                additionalData: '@import "./src/styles/variable.scss";',
+            },
+        },
+    },
 })
