ebda7c4a1ad407b1479d91671a4022b0c0cc1e71
guozhicheng <2399214024@qq.com>
2023-09-21 15:26:48 +0800

    build: 移除 antd

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.tsx'
-import zhCN from 'antd/locale/zh_CN'
-import {ConfigProvider} from 'antd'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ConfigProvider locale={zhCN}>
-            <App />
-        </ConfigProvider>
+        <App />
     </React.StrictMode>
 )
