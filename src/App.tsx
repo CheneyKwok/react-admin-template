dc745d388be2e90b2ca8bdeaa77e488b4a076864
guozhicheng <2399214024@qq.com>
2023-09-22 00:33:04 +0800

    feat: svg 通用组件封装

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ function App() {
     return (
         <>
             <h1>svg 测试</h1>
-            <SvgIcon name='phone' color='skyblue' />
+            <SvgIcon
+                name='phone'
+                color='red'
+                width='20px'
+                height='30px'
+                background-color='yellow'
+            />
         </>
     )
 }
--- src/components/SvgIcon.tsx
+++ src/components/SvgIcon.tsx
@@ -1,19 +1,30 @@
+import { CSSProperties } from 'react'
+
 interface SvgProps {
     name: string
     prefix?: string
     color?: string
-    props?: any
+    width?: string
+    height?: string
+    props?: object
 }
 const SvgIcon = ({
     name,
     prefix = 'icon',
     color = '#333',
+    width,
+    height,
     ...props
 }: SvgProps) => {
-    const symbolId = `#${prefix}-${name}`
+    const symbolId: string = `#${prefix}-${name}`
+    const style: CSSProperties | undefined = {
+        width,
+        height,
+        ...props,
+    }
     return (
         <>
-            <svg {...props}>
+            <svg {...props} style={style}>
                 <use href={symbolId} fill={color} />
             </svg>
         </>
