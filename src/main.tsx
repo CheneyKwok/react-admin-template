3391d7274030ba2ac2767fb6b208f5b2c1eac90c
guozhicheng <2399214024@qq.com>
2023-09-21 14:13:40 +0800

    style: pretty code

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from "@/App.tsx";
-import zhCN from 'antd/locale/zh_CN';
-import {ConfigProvider} from "antd";
+import App from '@/App.tsx'
+import zhCN from 'antd/locale/zh_CN'
+import {ConfigProvider} from 'antd'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
-            "@": path.resolve("./src") // 相对路径别名配置，使用 @ 代替 src
-        }
-    }
+            '@': path.resolve('./src'), // 相对路径别名配置，使用 @ 代替 src
+        },
+    },
 })
