0c762595777b290b1eac95808ee1bcd271b93466
guozhicheng <2399214024@qq.com>
2023-10-17 01:18:18 +0800

    feat: 面包屑功能

--- src/pages/layout/BasicLayout/BasicContent/index.tsx
+++ src/pages/layout/BasicLayout/BasicContent/index.tsx
@@ -1,17 +1,26 @@
+import { useMemo } from 'react'
 import { Breadcrumb, Layout, theme } from 'antd'
 import { Outlet } from 'react-router-dom'
 
+import useRoute from '@/hooks/useRoute.ts'
 import BasicTab from '@/pages/layout/BasicLayout/BasicContent/BasicTab'
 
 const BasicContent = () => {
+  const { matchedRoutes } = useRoute()
   const { token } = theme.useToken()
+
+  const items = useMemo(() => {
+    return matchedRoutes.map((route) => ({
+      // title: <Link to={route.fullPath || ''}>{route.meta?.title}</Link>,
+      title: route.meta?.title,
+      key: route.fullPath,
+    }))
+  }, [matchedRoutes])
+
+  console.log('items', items)
   return (
     <Layout style={{ padding: '0 24px 24px' }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
+      <Breadcrumb style={{ margin: '16px 0' }} items={items} />
       <Layout.Content
         style={{
           padding: 24,
